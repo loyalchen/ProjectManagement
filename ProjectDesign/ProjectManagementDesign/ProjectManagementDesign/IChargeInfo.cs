321270d482b0627112bbbbb14329027be682e4d7
TedForV <tedforv@outlook.com>
2016-08-25 16:13:37 +0800

    add class design project

---
+++ ProjectDesign/ProjectManagementDesign/ProjectManagementDesign/IChargeInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementDesign
+{
+    public interface IChargeInfo
+    {
+        int CreatorId { get; set; }
+        int OwnerId { get; set; }
+        int FormerOwnerId { get; set; }
+        DateTime ChargeFrom { get; set; }
+        DateTime? ChargeTo { get; set; }
+        DateTime FormerChargeFrom { get; set; }
+        DateTime FormerChargeTo { get; set; }
+    }
+}
---
+++ ProjectDesign/ProjectManagementDesign/ProjectManagementDesign/TimeControl.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementDesign
+{
+    public class TimeControl : ITimeControl
+    {
+        public int ACM
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public DateTime ATE
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public DateTime ATS
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int CEM
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public DateTime ETE
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public DateTime ETS
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+
+        public int SpanDays
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private int ActuallyCostMinutes()
+        {
+            throw new NotImplementedException();
+        }
+
+        private int CompareEstimated()
+        {
+            throw new NotImplementedException();
+        }
+
+        private int CalculateSpanDays()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
