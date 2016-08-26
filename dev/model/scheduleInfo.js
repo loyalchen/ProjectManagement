5c88b1d8320cf1c4bfed135dec703307d56bcf44
TedForV <tedforv@outlook.com>
2016-08-26 17:30:10 +0800

    add models

---
+++ dev/model/scheduleInfo.js
@@ -0,0 +1,38 @@
+import { Record } from 'immutable';
+import moment from 'moment';
+
+class ScheduleInfo extends Record({
+	ACM:0,
+	ATE:null,
+	ATS:null,
+	CEM:0,
+	ETE:null,
+	ETS:null,
+	SpanDays:0
+}){
+	/**
+	 * [calculateActualCostMinutes description]
+	 * @return {[type]} [description]
+	 */
+	calculateActualCostMinutes(){
+
+	}
+
+	/**
+	 * [compareMinutesWithEstimated description]
+	 * @return {[type]} [description]
+	 */
+	compareMinutesWithEstimated(){
+
+	}
+
+	/**
+	 * [calculateSpanDays description]
+	 * @return {[type]} [description]
+	 */
+	calculateSpanDays(){
+
+	}
+
+
+}
